f8a7dcf0c11cd0012d8e4ade522e7dfe17d0ad8e
diyor200 <abdulaxatovdiyorbek40@gmail.com>
2022-12-28 22:31:58 +0500

    Using mixins for class-based views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView,\
+    UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,\
+                                       PermissionRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Course
+
+
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin,
+                       LoginRequiredMixin,
+                       PermissionRequiredMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseEditMixin, ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    permission_required = 'courses.add_course'
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    permission_required = 'courses.change_course'
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    permission_required = 'courses.delete_course'
--- educa/urls.py
+++ educa/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
@@ -24,7 +24,8 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(),
          name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(),
-         name='logout')
+         name='logout'),
+    path('course/', include('courses.urls')),
 ]
 
 if settings.DEBUG:
