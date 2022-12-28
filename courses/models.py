1728b792f36ebe4e1bfbaa522daad77e70a24164
diyor200 <abdulaxatovdiyorbek40@gmail.com>
2022-12-28 21:15:37 +0500

    created a custom model field to order objects and implemented user authentication

--- courses/models.py
+++ courses/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
         ordering = ['title']
@@ -40,25 +41,32 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
 
 
 class Content(models.Model):
     module = models.ForeignKey(Module,
                                related_name='contents',
-                               on_delete=models.CASCADE,
-                               limit_choices_to={'model__in': (
-                                   'text',
-                                   'video',
-                                   'image',
-                                   'file'
-                               )})
+                               on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType,
-                                     on_delete=models.CASCADE)
+                                     on_delete=models.CASCADE,
+                                     limit_choices_to={'model__in':(
+                                     'text',
+                                     'video',
+                                     'image',
+                                     'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
@@ -85,8 +93,8 @@ class File(ItemBase):
 
 
 class Image(ItemBase):
-    image = models.ImageField(upload_to='images')
+       file = models.FileField(upload_to='images')
 
 
 class Video(ItemBase):
-    video = models.URLField()
+    url = models.URLField()
